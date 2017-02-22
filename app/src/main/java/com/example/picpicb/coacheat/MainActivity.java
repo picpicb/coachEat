f852241c59c52bbd2a8ed0db2270b40032f8b131
FRAU Jean-Michel <jean-michel.frau-llodra@etu.u-pec.fr>
2017-02-22 20:34:32 +0100

    Menu principale

--- app/src/main/java/com/example/picpicb/coacheat/MainActivity.java
+++ app/src/main/java/com/example/picpicb/coacheat/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.picpicb.coacheat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    static final RadioGroup.OnCheckedChangeListener ToggleListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(final RadioGroup radioGroup, final int i) {
+            for (int j = 0; j < radioGroup.getChildCount(); j++) {
+                System.out.println("toto");
+                final ToggleButton view = (ToggleButton) radioGroup.getChildAt(j);
+                view.setChecked(view.getId() == i);
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((RadioGroup) findViewById(R.id.ToggleGroup)).setOnCheckedChangeListener(ToggleListener);
     }
 }
