10c444f4f9cbfc1bddcc9825b7ae177cd7e302a8
ROMAIN JOLIVET <romain.jolivet@etu.u-pec.fr>
2017-03-05 15:33:41 +0100

    Affichage Info utilisateur ds textview différents

--- app/src/main/java/com/example/picpicb/coacheat/InfoUser.java
+++ app/src/main/java/com/example/picpicb/coacheat/InfoUser.java
@@ -21,6 +21,12 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 public class InfoUser extends AppCompatActivity {
     private Utilisateur user ;
     private TextView v ;
+    private TextView id;
+    private TextView nom;
+    private TextView pseudo;
+    private TextView Age;
+    private TextView Poids;
+    private TextView Taille;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,20 @@ public class InfoUser extends AppCompatActivity {
         System.out.println(user.toString());
 
         v = (TextView) findViewById(R.id.info1);
+        id = (TextView) findViewById(R.id.id);
+        nom = (TextView) findViewById(R.id.Nom);
+        pseudo = (TextView) findViewById(R.id.Pseudo);
+        Age = (TextView) findViewById(R.id.Age);
+        Taille = (TextView) findViewById(R.id.Taille);
+        Poids = (TextView) findViewById(R.id.Poids);
+
+        id.setText(Integer.toString(user.getId()));
+        nom.setText(user.getPrenom() + user.getNom());
+        pseudo.setText(user.getPseudo());
+        Age.setText(Integer.toString(user.getAge()));
+        Taille.setText(Integer.toString(user.getTaille()));
+        Poids.setText(Double.toString(user.getPoids()));
+
             v.setText(user.toString()); /*"\n" +*/
 
             ;
